89a34e0a3fde49bcf07168c91f7e50e35aa872da
Jana Zeller <33977318+Jana-Z@users.noreply.github.com>
2020-05-05 22:07:08 +0200

    Create map_on_debris.js

---
+++ free_code_camp/intermediate_algorithm_scripting/map_on_debris.js
@@ -0,0 +1,11 @@
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  return arr.map(x => {
+    return {'name': x['name'],
+      'orbitalPeriod': Math.round(
+        2*Math.PI
+        *Math.sqrt(Math.pow(earthRadius + x['avgAlt'], 3) / GM)
+      )}
+  });
+}
